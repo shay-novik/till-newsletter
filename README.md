8513820013dc9caf4dec527231ab2913dffd6853
Shay Novik <shay.novik@gmail.com>
2017-07-09 15:02:28 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# anafhadraha.github.io
+
+Anaf Hadraha's github newsletter website
+
+## Local Development
+
+Run these following commands to run the project locally:
+1. `npm install` to download and install all of the dependencies
+2. `gulp compress-images` if public/dist/img is empty
+2. `gulp dev` to run the dev server and tasks
+
+## Deployment to GitHub Pages
+
+TODO: Write deployment instructions
+
+## License (MIT)
+
+Copyright 2017 'Mador Till'
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// Dependencies
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const wait = require('gulp-wait');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant')
+const browserSync = require('browser-sync').create();
+
+// Important folder paths
+const PATHS = {
+  'public': './public',
+  'src': {
+    'scss': './src/scss',
+    'images': './src/images'
+  },
+  'dist': {
+    'css': `./public/dist/css`,
+    'js': `./public/dist/js`,
+    'img': './public/dist/img'
+  }
+}
+
+// Compile sass, autoprefix it and concat the files into one bundle
+gulp.task('bundle-css', () => {
+  // Watch files and rerun on change/add/delete
+  return watch(`${PATHS.src.scss}/**/*.scss`, {
+    ignoreInitial: false
+  }, () => {
+    gulp.src(`${PATHS.src.scss}/main.scss`)
+      .pipe(wait(500)) // Fixes file not found error on vscode
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(concat('bundle.css'))
+      .pipe(gulp.dest(`${PATHS.dist.css}`));
+  })
+});
+
+// Compress images
+gulp.task('compress-images', () => {
+  gulp.src(`${PATHS.src.images}/*`)
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(`${PATHS.dist.img}`));
+});
+
+// Use browser-sync to serve files over static server
+gulp.task('browser-sync', () => {
+  // Browser-sync properties
+  browserSync.init({
+    'notify': false,
+    'server': {
+      'baseDir': './public'
+    }
+  });
+  // Reload browser when files in public folder change
+  gulp.watch(`${PATHS.public}/**/*`).on('change', browserSync.reload);
+});
+
+// Master task that bundles every other development task
+gulp.task('dev', ['bundle-css', 'browser-sync']);
